7858e11544fdb914dc30b0cec65c2b87731d1ac6
chancellor carlton <chancellorcarlton@chancellors-MacBook-Pro.local>
2022-01-17 12:10:26 -0700

    Third Commit

--- server/index.js
+++ server/index.js
@@ -1,7 +1,14 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 
 const app = express();
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, '../client/index.html'));
+});
+
+app.use(express.static('client')) //linking public folder
 
 app.use(cors());
 app.use(express.json());
