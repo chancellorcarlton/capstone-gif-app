9de42bfe42a9cf8dc9abf2deb238f1d08544a4aa
chancellor carlton <chancellorcarlton@chancellors-MacBook-Pro.local>
2022-01-17 11:57:59 -0700

    First Commit

---
+++ server/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const ctrl = require('./controller')
+
+
+
+
+
+const port = process.env.PORT || 4321;
+
+app.listen(port, () => {
+    console.log(`Listening on ${port}`)
+});
